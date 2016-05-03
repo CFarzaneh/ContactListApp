b5a86278fe722b7294f36e774888c0243fe10973
Cameron <cameron.farzaneh@gmail.com>
2016-05-02 20:49:44 -0400

    Add contact button and store data in mongodb database

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function (req, res) {
 	console.log("I received a GET request")
 
-	person1 = {
-		name: 'Tim',
-		email: 'tim@email.com',
-		number: '1111111111'
-	};
-	person2 = {
-		name: 'Rob',
-		email: 'rob@email.com',
-		number: '3331414141'
-	};
-	person3 = {
-		name: 'Juan',
-		email: 'Juan@email.com',
-		number: '1312323123'
-	};
+	db.contactlist.find(function (err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
+});
 
-	var contactlist = [person1, person2, person3];
-	res.json(contactlist);
+app.post('/contactlist', function (req, res) {
+	console.log(req.body);
+	db.contactlist.insert(req.body, function(err, doc) {
+		res.json(doc);
+	});
 });
 
 app.listen(3000);
